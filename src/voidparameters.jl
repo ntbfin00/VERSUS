5a7995ddca6760ac6ca24d3ae80be18f7cf8f6fa
ntbfin00 <nathan.findlay@port.ac.uk>
2023-03-29 09:48:15 +0100

    Moved read_input to utils.jl, moved GalaxyCatalogue to voidparameters.jl

--- src/voidparameters.jl
+++ src/voidparameters.jl
@@ -1,5 +1,5 @@
 module VoidParameters
-export Cosmology, MeshParams, SphericalVoidParams
+export Cosmology, GalaxyCatalogue, MeshParams, SphericalVoidParams
 
 using Parameters
 
@@ -13,9 +13,16 @@ using Parameters
     bias::Float64 = 2.  # galaxy bias 
 end
 
+@with_kw mutable struct GalaxyCatalogue
+    gal_pos::Array{AbstractFloat,2}
+    gal_wts::Array{AbstractFloat,1} = ones(size(gal_pos,1))
+    rand_pos::Array{AbstractFloat,2} = Array{AbstractFloat}(undef,0,0)
+    rand_wts::Array{AbstractFloat,1} = ones(size(rand_pos,1))
+end
+
 @with_kw mutable struct MeshParams
     dtype::String = "f4"
-    nbins_vf::Int = 0  # number of bins for voidfinding (calculated using galaxy density if 0)
+    nbins_vf::Array{Int,1} = [0]  # number of bins for voidfinding (calculated using galaxy density if 0)
     is_box::Bool = true  # set to false for survey-like data
     box_length::Float64 = 1500.  # box length [Mpc/h] (disregarded if is_box=false)
     box_centre::Array{Float64,1} = fill(box_length/2, 3) # box centre (disregarded if is_box=false)
@@ -26,12 +33,11 @@ end
     # reconstruction parameters
     recon_alg::String = "IFFTparticle"
     r_smooth::Float64 = 10. # smoothing scale [Mpc/h]
-    nbins_recon::Int = 0  # number of bins for reconstruction (calculated using r_smooth if 0)
+    nbins_recon::Array{Int,1} = [0]  # number of bins for reconstruction (calculated using r_smooth if 0)
     los::String = "z"  # line-of-sight axis of box (disregarded if is_box=false)
 end
 
 @with_kw mutable struct SphericalVoidParams
-    verbose::Bool = false
     radii::Array{Float64,1} = [0]  # void radii [Mpc/h] (calculated using galaxy density if 0)
     min_dens_cut::Float64 = 1.0
     max_overlap_frac::Float64 = 0.
