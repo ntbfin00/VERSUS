92ce2223f63fcade2d32e34636c9978b2e302597
ntbfin00 <nathan.findlay@port.ac.uk>
2023-04-04 15:01:11 +0100

    tidying up

--- src/utils.jl
+++ src/utils.jl
@@ -1,16 +1,6 @@
 module Utils
 export setup_logging, read_input
 
-# using LoggingExtras, Dates
-
-# const date_format = "yyyy-mm-dd HH:MM:SS"
-
-# timestamp_logger(logger) = TransformerLogger(logger) do log
-    # merge(log, (; message = "$(Dates.format(now(), date_format)) $(log._module) $(log.message)"))
-# end
-
-# ConsoleLogger(stdout, Logging.Debug) |> timestamp_logger |> global_logger
-
 include("voidparameters.jl")
 
 using .VoidParameters
@@ -139,51 +129,5 @@ function read_input(fn::String, build_mesh::Bool, data_format::String, data_cols
     end
 
 end
-
-
-# """
-# Return input parameters specific to spherical voids.
-# """
-# function get_params(cat::Main.MeshBuilder.GalaxyCatalogue, mesh::Main.VoidParameters.MeshParams, par::Main.VoidParameters.SphericalVoidParams)
-    # println("\n ==== Spherical void input parameters (galaxy field) ==== ")
-
-    # params = Dict(key => getfield(par, key) for key ∈ fieldnames(Main.VoidParameters.SphericalVoidParams))
-
-    # # determine default voidfinding bins
-    # if mesh.nbins_vf == 0
-        # r_sep = mean_gal_sep(cat, mesh)
-        # nbins = rec.boxsize[1]/(0.5 * r_sep)
-        # params[:nbins_vf] = optimal_binning(nbins, "above")
-    # else
-        # params[:nbins_vf] = mesh.nbins_vf
-    # end
-
-    # # determine default radii
-    # if par.radii = [0]
-        # params[:radii] = [2:10;] * mean_gal_sep(cat, mesh)
-    # end
-
-    # for (key, value) in pairs(params)
-        # println(key, " = ", value)
-    # end
-
-    # # for field in propertynames(params)
-            # # println(field, " = ", getfield(params, field))
-    # # end 
-
-# end
-
-# function get_params(delta::Array{<:AbstractFloat,3}, par::Main.VoidParameters.SphericalVoidParams)
-    # println("\n ==== Spherical void input parameters (density field) ==== ")
-
-    # params = Dict(key => getfield(par, key) for key ∈ fieldnames(Main.VoidParameters.SphericalVoidParams))
-
-    # params[:nbins_vf] = size(delta,1)
-    
-    # for (key, value) in pairs(params)
-        # println(key, " = ", value)
-    # end
-
-# end
 
 end
